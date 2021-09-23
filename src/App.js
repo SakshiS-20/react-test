ccb958a91a980e353652a87eaa98651f04380ee5
Sakshi <sakshis@shriffle.com>
2021-09-23 20:35:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import ImagesList from './components/images-list';
+import ImagesTable from './components/images-table';
+
+function App() {
+  return (
+    <div>
+      <ImagesList></ImagesList>
+      <ImagesTable></ImagesTable>
+    </div>
+  );
+}
+
+export default App;
